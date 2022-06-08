ee883b37eae04fe819be22c4143dcd8d005e85a9
Anna Chan <annachan200@gmail.com>
2022-06-08 17:04:36 +0100

    issue with updating feed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const express = require ('express');
+const { Cat } = require ('./models');
+const cats = require('./models/cats');
+
 const app = express();
 
 // we expect to have to parse json from request bodies, 
@@ -11,9 +14,42 @@ app.get('/', (req, res) => {
 })
 
 app.post('/cats', (req, res) => {
-    res.status(201).json('request created')
+    Cat.create(req.body)
+    // Cat.create({name, breed, lastFed})
+    .then(cat => res.status(201).json(cat))
+    .catch(cat => res.status(400).json('error creating cat'))
 })
 
+app.get('/cats', (req, res) => {
+    // Cat.findAll(req.body)
+    Cat.findAll({ where: req.query})
+    .then(cat => res.status(200).json(cat))
+    .catch(cat => res.status(400).json('error finding all cats'))
+})
 
+app.get('/cats/:catId', (req, res) => {
+    Cat.findByPk(req.params.catId)
+    // Cat.findByPk({ where: req.params.catId })
+    .then(cat => res.status(200).json(cat))
+    .catch(cat => res.status(400).json('error finding cat by Id'))
+})
+
+app.patch('/cats/:catId', (req, res) => {
+    Cat.update(req.body, { where: { id: req.params.catId } })
+    .then(cat => res.status(200).json(cat))
+    .catch(cat => res.status(400).json('error updating cat'))
+})
+
+app.delete('/cats/:catId', (req, res) => {
+    Cat.destroy({ where: { id: req.params.catId } })
+    .then(cat => res.status(200).json(cat))
+    .catch(cat => res.status(400).json('error deleting cat'))
+})
+
+app.patch('/feed/:catId', (req, res) => {
+    Cat.update({ lastFed: new Date() }, { where: { id: req.params.catId } })
+    .then(cat => res.status(200).json(cat))
+    .catch(cat => res.status(400).json('error updating when cat was fed'))
+})
 
 module.exports = app
